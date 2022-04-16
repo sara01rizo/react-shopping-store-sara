c0a3551c12a9aa0e08a66d802e8c544bc3ce87c6
sara-pulido <sara01rizo@gmail.com>
2022-04-17 01:31:42 +0200

    Create file Products.js and update App.js to create a constructor

--- react-shopping-store-sara/src/App.js
+++ react-shopping-store-sara/src/App.js
@@ -1,19 +1,34 @@
 // feature 1
+import React from "react";
+import data from "./data.json"
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">Reat Shopping Store SARA</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">Products</div>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>
         All rigth is reserved.
       </footer>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
