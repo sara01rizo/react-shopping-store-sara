5214bba8cb995198186d71dfa216b4fbf99e7d4a
sara-pulido <sara01rizo@gmail.com>
2022-04-18 09:09:29 +0200

    Update: assests images for the six products

--- react-shopping-store-sara/src/App.js
+++ react-shopping-store-sara/src/App.js
@@ -1,5 +1,6 @@
 // feature 1
 import React from "react";
+import Products from "./components/Products";
 import data from "./data.json"
 
 class App extends React.Component {
@@ -19,7 +20,9 @@ class App extends React.Component {
       </header>
       <main>
         <div className="content">
-          <div className="main">Products</div>
+          <div className="main">
+            <Products products={this.state.products}></Products>
+          </div>
           <div className="sidebar">Cart Items</div>
         </div>
       </main>
