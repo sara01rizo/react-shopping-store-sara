82cecec67093438c27cea22e2a5f4ec56a89c3a8
sara-pulido <sara01rizo@gmail.com>
2022-05-02 22:52:41 +0200

    Update: Footer add Filter to the end

--- react-shopping-store-sara/src/components/Footer.js
+++ react-shopping-store-sara/src/components/Footer.js
@@ -6,7 +6,31 @@ export default class Footer extends Component {
     return (
       <div>
           <div className='footer'>
-              <p className='footer'>All rigth is reserved </p>
+              
+          <div className='filter'>
+          <div className='filter-result'>{this.props.count}</div>
+          <div className='filter-sort'>
+              Order {" "}
+            <select>
+                <option>Latest</option>
+                <option value="lowest">Lowest</option>
+                <option value="highest">Highest</option>
+            </select>
+           </div>
+          <div className='filter-size'> 
+            Filter 
+            <select>
+              <option value="">ALL</option>
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+              <option value="XXL">XXL</option>
+              </select>
+              </div>
+          </div>
+            <p className='footer'>All rigth is reserved </p>
           </div>
       </div>
     )
